12317bf851500aec277084be899bd5dd21938e06
Pedro Silva <mrgpedrosilva@gmail.com>
2025-11-18 00:29:24 -0300

    feat: esc to exit puzzle scene

--- Assets/Scripts/Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PuzzleManager : MonoBehaviour
 {
     public static PuzzleManager Instance;
+    private InputAction _action;
 
     private int totalPieces;
     private int placedPieces = 0;
@@ -11,6 +13,22 @@ public class PuzzleManager : MonoBehaviour
     void Awake()
     {
         Instance = this;
+        _action = new InputAction(binding: "<Keyboard>/Escape", type: InputActionType.Button);
+        _action.Enable();
+    }
+
+    private void Update()
+    {
+        if (_action.triggered)
+        {
+            // Reativa a UI do jogo
+            GameObject principalUI = GameObject.Find("CanvasPrincipal");
+            if (principalUI != null)
+                principalUI.SetActive(true);
+
+            // Fecha a cena do puzzle
+            SceneManager.UnloadSceneAsync(gameObject.scene.name);
+        }
     }
 
     public void SetTotalPieces(int total)
